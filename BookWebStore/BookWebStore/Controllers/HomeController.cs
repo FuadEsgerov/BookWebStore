1bc254a74056bd9eb91c4351686c6c7f6b3b8a2d
FuadEsgerov <56359250+FuadEsgerov@users.noreply.github.com>
2020-03-10 21:49:49 +0400

    Migration
    dotnet ef --startup-project .\BookWebStore\BookWebStore.csproj Migrations add InitialCreate -p .\Repository\Repository.csproj
    
    dotnet ef --startup-project .\BookWebStore\BookWebStore.csproj database update -p .\Repository\Repository.csproj

--- BookWebStore/BookWebStore/Controllers/HomeController.cs
+++ BookWebStore/BookWebStore/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using BookWebStore.Models;
+
 
 namespace BookWebStore.Controllers
 {
@@ -23,15 +23,5 @@ namespace BookWebStore.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
